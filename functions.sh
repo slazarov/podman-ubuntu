e191931e08763f7b34f30988ce2a6a9fb4793bd1
luckylinux <luckylinux>
2025-02-14 23:40:24 +0100

    Git Tag sort by Creation Date.

--- functions.sh
+++ functions.sh
@@ -5,7 +5,11 @@ get_latest_tag() {
     # ...
 
     # List all Tags excluding rc Patterns
-    latest=$(git tag --list --sort -tag | grep -v rc | head -n1)
+    # This seems to Fail on 1.14 being latest -> 1.9 being used e.g. on fuse-overlayfs
+    # latest=$(git tag --list --sort -tag | grep -v rc | head -n1)
+
+    # This seems to do better
+    latest=$(git tag --list --sort -creatordate | grep -v rc | head -n1)
 
     # Return Result
     echo "${latest}"
