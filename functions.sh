17bc00db817810b362e265b41f867f12fbfe1f80
luckylinux <luckylinux>
2025-02-15 11:32:12 +0100

    Use sort --version-sort to get latest Tag Released.

--- functions.sh
+++ functions.sh
@@ -9,7 +9,10 @@ get_latest_tag() {
     # latest=$(git tag --list --sort -tag | grep -v rc | head -n1)
 
     # This seems to do better
-    latest=$(git tag --list --sort -creatordate | grep -v rc | head -n1)
+    # latest=$(git tag --list --sort -creatordate | grep -v rc | head -n1)
+
+    # Take the latest highest stable Version release
+    latest=$(git tag --list --sort -creatordate | grep -v rc | grep -E ^v | sort --reverse --version-sort | head -n1)
 
     # Return Result
     echo "${latest}"
