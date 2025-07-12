279c97b22b4faf8b6cb033258955ac7d3f538c84
luckylinux <luckylinux>
2025-07-12 13:07:06 +0200

    Fix include Path for Configuration File.

--- functions.sh
+++ functions.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# Determine toolpath if not set already
+relativepath="./" # Define relative path to go from this script to the root level of the tool
+if [[ ! -v toolpath ]]; then scriptpath=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ); toolpath=$(realpath --canonicalize-missing ${scriptpath}/${relativepath}); fi
+
 # Load Configuration
-source config.sh
+source "${toolpath}/config.sh"
 
 get_latest_tag() {
     # Input Parameters
