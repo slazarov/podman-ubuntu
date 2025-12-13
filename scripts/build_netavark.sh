053b9210a107e720352fe4a544ec06401b73c4a7
luckylinux <luckylinux>
2025-12-13 12:38:20 +0100

    Move Cargo profile include before Configuration include.

--- scripts/build_netavark.sh
+++ scripts/build_netavark.sh
@@ -7,6 +7,10 @@
 relativepath="../" # Define relative path to go from this script to the root level of the tool
 if [[ ! -v toolpath ]]; then scriptpath=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ); toolpath=$(realpath --canonicalize-missing ${scriptpath}/${relativepath}); fi
 
+# Required Fix otherwise go complains about 1.22.6 vs 1.23 mismatch
+. "$HOME/.cargo/env"
+#export PATH="CUSTOMPATH:$PATH"
+
 # Load Configuration
 source "${toolpath}/config.sh"
 
@@ -15,10 +19,6 @@ source "${toolpath}/functions.sh"
 
 # Change Folder to Build Root
 cd "${BUILD_ROOT}" || exit
-
-# Required Fix otherwise go complains about 1.22.6 vs 1.23 mismatch
-. "$HOME/.cargo/env"
-#export PATH="CUSTOMPATH:$PATH"
 
 git_clone_update https://github.com/containers/netavark netavark
 cd netavark
