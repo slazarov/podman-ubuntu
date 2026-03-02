457954e91c21ec81a73245e3aa663f797bc71764
slazarov <s.lazarov@lp-sofia.com>
2026-03-02 21:14:23 +0200

    feat(04-02): add step-level progress to build_podman.sh
    - Add log_build_output for build logging
    - Wrap operations in step_start/step_done calls:
      - Cloning repository
      - Checking out tag
      - Logging version
      - Applying pre-build fixes
      - Building
      - Installing
      - Post-install configuration
    - Use run_logged for make commands to suppress console output

--- scripts/build_podman.sh
+++ scripts/build_podman.sh
@@ -19,34 +19,43 @@ trap 'error_handler $? $LINENO "$BASH_SOURCE"' ERR
 # Change Folder to Build Root
 cd "${BUILD_ROOT}" || exit
 
+# Initialize build logging
+log_build_output "podman"
+
 # Fix for cloud-init where GOCACHE, XDG_CACHE_HOME, and HOME are not set
 export GOCACHE="${GOCACHE:-/tmp/go-build}"
 export XDG_CACHE_HOME="${XDG_CACHE_HOME:-/tmp}"
 export HOME="${HOME:-/root}"
 
-# Might actually not be needed for build-only (more for Troubleshooting)
-# go install golang.org/x/tools/gopls@latest
-
 # Required Fix otherwise go complains about 1.22.6 vs 1.23 mismatch
 export PATH="$GOPATH:$PATH"
 
+step_start "Cloning repository"
 git_clone_update https://github.com/containers/podman.git podman
 cd "${BUILD_ROOT}/podman"
+step_done
+
+step_start "Checking out tag"
 git_checkout "${PODMAN_TAG}"
+step_done
 
-# Log Component
+step_start "Logging version"
 log_component "podman"
+step_done
 
-# Must Patch 1.22.6 -> 1.23 in /usr/src/podman/podman/go.mod
+step_start "Applying pre-build fixes"
 sed -Ei "s|^go 1.22.6$|go 1.23|" go.mod
+step_done
 
-#make BUILDTAGS="selinux seccomp apparmor systemd" PREFIX=/usr
-make GO="$GOPATH/go" BUILDTAGS="seccomp apparmor systemd" PREFIX=/usr
-sudo make GO="$GOPATH/go" install PREFIX=/usr
+step_start "Building"
+run_logged make GO="$GOPATH/go" BUILDTAGS="seccomp apparmor systemd" PREFIX=/usr
+step_done
 
-# Copy containers.conf to enable podman to find helper binaries in /usr/local/bin
+step_start "Installing"
+run_logged sudo make GO="$GOPATH/go" install PREFIX=/usr
+step_done
+
+step_start "Post-install configuration"
 sudo mkdir -p /etc/containers
 sudo cp "${toolpath}/config/containers.conf" /etc/containers/containers.conf
-
-# Copy to Target Folder
-
+step_done
